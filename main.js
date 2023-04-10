fe67bebbdcb9472a0d399b5db918ec740075e473
Rebecca <prebecca118@yahoo.co.uk>
2023-04-10 15:40:18 +0100

    example function written and plan of tests made

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 /* 
-👉 Write your kata here!
+The Kata will ask users to write a function that finds the number of days between two inputted dates.
+NOTES:
+- It should not matter which order the dates are inputted in
+- If the inputted date is not valid, the function should return 'please input a date in the form yyyy-mm-dd'
+- If a date is entered in a valid form (yyyy-mm-dd), the number of days and the month will be valid (e.g. the 31st of Feburary would not be inputted)
+- The new Date() function is specific to the millisecond so a conversion must be done to get the answer in days
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function daysBetween(dateStr1, dateStr2) {
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  const millisecInOneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+
+if (dateRegex.test(dateStr1) && dateRegex.test(dateStr2)) {
+  const firstDate = new Date(dateStr1);
+  const secondDate = new Date(dateStr2);
+
+  const diffDays = Math.round(Math.abs((firstDate - secondDate) / millisecInOneDay));
+
+  return `The number of days between the two dates is ${diffDays}`;
+}
+else {
+    return `The inputted dates should be in the form 'yyyy-mm-dd`
+}
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,12 @@
 //👉 Write your tests below here:
+
+/* SOME TEST SCENARIOS:
+
+1. Does the function work when two valid dates are iputted?
+2. Does it work whe first date is before the second AND when it is after?
+3. When there are no days between
+4. When there is one day between
+5. When there are many days between
+6. When the first, second or both of the input dates are invalid
+
+*/
